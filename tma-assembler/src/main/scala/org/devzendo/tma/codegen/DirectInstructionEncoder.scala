c1e757d77c82df95f2163bb47f00ad6546069c61
Matt Gumbley <matt.gumbley@devzendo.org>
2018-07-13 18:22:35 +0100

    Add a direct instruction encoder.

---
+++ tma-assembler/src/main/scala/org/devzendo/tma/codegen/DirectInstructionEncoder.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2008-2018 Matt Gumbley, DevZendo.org http://devzendo.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.devzendo.tma.codegen
+
+import scala.collection.mutable.ListBuffer
+
+object DirectInstructionEncoder {
+
+    private val PFIX: Int = 0x20
+    private val NFIX: Int = 0x60
+
+    // Given a direct instruction with its opcode in its left-hand nybble, and a value for its arg, encode the
+    // shortest sequence of (pfix|nfix)*<opcode> bytes that encode the direct instruction.
+    // Algorithm converted from "The Transputer Handbook", Graham & King, p24.
+    def apply(op: Int, arg: Int): List[Int] = {
+        val prefixed = if (arg < 0) {
+            encodeStep((~arg) >> 4, true)
+        } else {
+            if (arg > 15) {
+                encodeStep(arg >> 4, false)
+            } else {
+                new ListBuffer[Int]()
+            }
+        }
+        val oparg = op | (arg & 0x0f)
+        prefixed += oparg
+        prefixed.toList
+    }
+
+    private def encodeStep(arg: Int, negative: Boolean): ListBuffer[Int] = {
+        if (arg > 15) {
+            encodeStep(arg >> 4, negative) += (PFIX | (if (negative) {
+                ~arg
+            } else {
+                arg
+            }) & 0x0f)
+        } else {
+            val fix = if (negative) {
+                NFIX
+            } else {
+                PFIX
+            }
+            new ListBuffer[Int]() += (fix | (arg & 0x0f))
+        }
+    }
+}
---
+++ tma-assembler/src/test/scala/org/devzendo/tma/codegen/TestDirectInstructionEncoder.scala
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2008-2018 Matt Gumbley, DevZendo.org http://devzendo.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.devzendo.tma.codegen
+
+import org.junit.Test
+import org.scalatest.MustMatchers
+import org.scalatest.junit.AssertionsForJUnit
+
+class TestDirectInstructionEncoder extends AssertionsForJUnit with MustMatchers {
+    import DirectInstructionEncoder._
+    val logger = org.log4s.getLogger
+
+    private val LDC = 0x40 // Other direct instructions are available; they all encode in the same way.
+
+    def ldc(arg: Int): List[Int] = {
+        val ints = apply(LDC, arg)
+        val intsHex = ints map { i: Int => f"0x$i%02x" } mkString " "
+        logger.info(f"Arg 0x$arg%08x: $intsHex%s")
+        logger.info(f"Arg $arg%d: $ints%s")
+        ints
+    }
+
+    @Test
+    def positiveNybble(): Unit = {
+        ldc(0x0c) must be (List(0x4c))
+    }
+
+    @Test
+    def positiveByte(): Unit = {
+        ldc(0x28) must be (List(0x22, 0x48))
+    }
+
+    @Test
+    def positiveWord(): Unit = {
+        ldc(257) must be (List(0x21, 0x20, 0x41))
+    }
+
+    @Test
+    def positiveDWord(): Unit = {
+        ldc(0x1234abcd) must be (List(0x21, 0x22, 0x23, 0x24, 0x2a, 0x2b, 0x2c, 0x4d))
+    }
+
+    @Test
+    def positiveMaxDWord(): Unit = {
+        ldc(0x7FFFFFFF) must be (List(0x27, 0x2f, 0x2f, 0x2f, 0x2f, 0x2f, 0x2f, 0x4f))
+    }
+
+    @Test
+    def negativeNybble(): Unit = {
+        ldc(-0x0c) must be (List(0x60, 0x44))
+    }
+
+    @Test
+    def negativeByte(): Unit = {
+        ldc(-0x28) must be (List(0x62, 0x48))
+    }
+
+    @Test
+    def negativeWord(): Unit = {
+        ldc(-257) must be (List(0x61, 0x2f, 0x4f))
+    }
+
+    @Test
+    def negativeMaxDWord(): Unit = {
+        ldc(0x80000000) must be (List(0x67, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x40))
+    }
+}
